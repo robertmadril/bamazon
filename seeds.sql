a814807ddef324bd000a018c6afdfc7bcaf4d22d
robertmadril <robertmmadril@gmail.com>
2019-04-30 18:41:05 -0700

    schema and seeds did not commit

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Roomba", "Electronics", 99.99, 15), ("Google Home", "Electronics", 120.50, 8), ("MacBook Pro", "Electronics", 1099.99, 2), ("iPhone", "Electronics", 599.99, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hiking Boots", "Clothing", 89.99, 20), ("T-Shirt", "Clothing", 15.99, 45), ("Socks", "Clothing", 4.99, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bone", "Pet Supplies", 10.99, 40), ("Treats", "Pet Supplies", 15.99, 30), ("Leash", "Pet Supplies", 25.99, 13);
